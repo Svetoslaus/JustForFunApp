a2e96d433ac0f93e49bfe21651aedbf48857de4f
Svetoslaus <svetoslavshterev@Svetoslavs-Air.fritz.box>
2023-05-01 12:02:04 +0200

    basic style template

--- App.js
+++ App.js
@@ -1,20 +1,60 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import {View, Text, SafeAreaView, StyleSheet } from 'react-native';
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <SafeAreaView style={styles.safeArea}>
+    <View style={styles.container1}>
+      <Text style={styles.text}>orange</Text>
     </View>
-  );
+    <View style={styles.container2}>
+      <Text style={styles.text}>magenta</Text>
+    </View>
+    <View style={styles.container3}>
+      <Text style={styles.text}>blue</Text>
+    </View>
+    <View style={styles.container4}>
+      <Text style={styles.text}>cyan</Text>
+    </View>
+    </SafeAreaView>
+  )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
+   container1: {
+    backgroundColor: '#cb4b16',
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding:10,
+    
+   },
+   container2: {
+    backgroundColor: '#d33682',
     alignItems: 'center',
     justifyContent: 'center',
-  },
+    padding:10,
+   },
+   container3: {
+    backgroundColor: '#268bd2',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding:10,
+   },
+   container4: {
+    backgroundColor: '#2aa198',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding:10,
+   },
+   safeArea: {
+    flex: 1
+   },
+   text: {
+    color: 'white',
+    
+   }
 });
+
+
+
+export default App;
